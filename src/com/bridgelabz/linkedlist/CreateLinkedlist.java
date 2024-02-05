afbe710d0816a0df68791ecb2322f5ab9ae16624
mk3709 <mk3709@srmist.edu.in>
2024-02-05 14:36:20 +0530

    linkedListAdded

--- src/com/bridgelabz/linkedlist/CreateLinkedlist.java
+++ src/com/bridgelabz/linkedlist/CreateLinkedlist.java
@@ -1,14 +1,57 @@
 package com.bridgelabz.linkedlist;
 import java.util.LinkedList;
 
+class MyNode{
+    int data;
+     MyNode next;
+    public  MyNode(int data)
+    {
+        this.data=data;
+        this.next=null;
+
+    }
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
 
 
+
+}
 
 public class CreateLinkedlist {
     public static void main(String[] args) {
 
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyNode   myFirstNode = new MyNode(70);
+        MyNode   mySecondNode = new MyNode(30);
+        MyNode   myThirdNode = new MyNode(56);
+
+        myFirstNode.next=mySecondNode;
+        mySecondNode.next=myThirdNode;
+
+
+        LinkedList<MyNode> l1 = new LinkedList<>();
+        l1.add(myFirstNode);
+        l1.add(mySecondNode);
+        l1.add(myThirdNode);
+
+        System.out.println("LinkedList " + getNodeSequence(l1));
     }
+
+    private  static String getNodeSequence(LinkedList<MyNode> linkedList)
+    {
+         StringBuilder sequence = new StringBuilder();
+
+      for(MyNode node:linkedList)
+      {
+          sequence.append(node.data).append("->");
+
+      }
+
+      sequence.delete(sequence.length()-2,sequence.length());
+      return sequence.toString();
+}
 }
