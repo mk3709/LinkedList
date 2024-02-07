5c2cab9d0d23e777a0d3d1e4a6e4e0ba90aae1c8
mk3709 <mk3709@srmist.edu.in>
2024-02-07 10:21:34 +0530

    RefactoringDone

--- src/com/bridgelabz/linkedlist/CreateLinkedlist.java
+++ src/com/bridgelabz/linkedlist/CreateLinkedlist.java
@@ -1,10 +1,10 @@
 package com.bridgelabz.linkedlist;
 import java.util.LinkedList;
 
-class MyNode{
-    int data;
-     MyNode next;
-    public  MyNode(int data)
+class MyNode<T>{
+    T data;
+     MyNode<T> next;
+    public  MyNode(T data)
     {
         this.data=data;
         this.next=null;
@@ -14,36 +14,30 @@ class MyNode{
         return next;
     }
 
-    public void setNext(MyNode next) {
+    public void setNext(MyNode<T> next) {
         this.next = next;
     }
-
-
-
 }
 
-public class CreateLinkedlist {
-     MyNode head;
 
+public class CreateLinkedlist {
+    MyNode<Integer> head;
     public static void main(String[] args) {
 
-        MyNode myFirstNode = new MyNode(56);
-        MyNode mySecondNode = new MyNode(30);
-        MyNode myThirdNode = new MyNode(70);
+        CreateLinkedlist linkedList = new CreateLinkedlist();
+
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
 
         //56-30->70
         myFirstNode.next = mySecondNode;
         mySecondNode.next = myThirdNode;
-        insertNodeBetween(myFirstNode, mySecondNode, myThirdNode);
+        insertNodeBetween(myFirstNode, new MyNode<>(40), myThirdNode);
 
-        LinkedList<MyNode> l1 = new LinkedList<>();
-        l1.add(myFirstNode);
-        l1.add(mySecondNode);
-        l1.add(myThirdNode);
+        LinkedList<MyNode<Integer>> l1 = new LinkedList<>();
         pop(l1);
-        System.out.println("LinkedList " + getNodeSequence(l1));
     }
-
 
     public void insert(int data) {
         MyNode newNode = new MyNode(data);
@@ -57,9 +51,8 @@ public class CreateLinkedlist {
             temp.next = newNode;
         }
     }
-
-    public MyNode search(int key) {
-        MyNode current = head;
+    public MyNode<Integer> search(int key) {
+        MyNode<Integer> current = head;
         while (current != null) {
             if (current.data == key) {
                 return current;
@@ -69,22 +62,23 @@ public class CreateLinkedlist {
         return null;
     }
 
-    public void insertAfter(MyNode preNode, int newData) {
+    public void insertAfter(MyNode<Integer> preNode, int newData) {
         if (preNode == null) {
             System.out.println("Previous node not null");
             return;
         }
-        MyNode newNode = new MyNode(newData);
+        MyNode<Integer> newNode = new MyNode<>(newData);
         newNode.next = preNode.next;
         preNode.next = newNode;
 
 
     }
 
+
     public void delete(int key)
     {
-        MyNode current = head;
-        MyNode prev=null;
+        MyNode<Integer> current = head;
+        MyNode<Integer> prev=null;
 
         if(current!=null && current.data ==key)
         {
@@ -103,36 +97,71 @@ public class CreateLinkedlist {
         prev.next=current.next;
     }
 
-    public int size()
-    {
-        int count=0;
-        MyNode current = head;
-        while(current!=null)
-        {
+    public int size() {
+        int count = 0;
+        MyNode<Integer> current = head;
+        while (current != null) {
             count++;
-            current=current.next;
+            current = current.next;
         }
         return count;
     }
 
-    private static void insertNodeBetween(MyNode previousNode, MyNode newNode, MyNode nextNode) {
+    private static void insertNodeBetween(MyNode<Integer> previousNode, MyNode newNode, MyNode nextNode) {
         previousNode.next = newNode;
         newNode.next = nextNode;
-
     }
 
 
-//   // private static void pop(LinkedList<MyNode> linkedList)
+    private static void pop(LinkedList<MyNode<Integer
+            >> LinkedList) {
+        LinkedList.removeLast();
+    }
+
+    static class SortedLinkedList<T extends Comparable<T>> {
+        private MyNode<T> head;
+
+        public void add(T data) {
+            MyNode<T> newNode = new MyNode(data);
+            if (head == null || head.data.compareTo(data) > 0) {
+                newNode.next = head;
+                head = newNode;
+            } else {
+                MyNode<T> current = head;
+                while (current.next != null && current.next.data.compareTo(data) < 0) {
+                    current = current.next;
+                }
+                newNode.next = current.next;
+                current.next = newNode;
+            }
+        }
+
+        public void display() {
+            MyNode<T> current = head;
+            while (current != null) {
+                System.out.println(current.data + "->");
+                current = current.next;
+            }
+            System.out.print("null");
+        }
+    }
+
+
+
+
+
+
+//
+//
+//    private static void pop(LinkedList<MyNode> linkedList)
 //    {
 //        linkedList.removeFirst();
 //    }
 
-    private static void pop(LinkedList<MyNode> LinkedList) {
-        LinkedList.removeLast();
-    }
 
 
-    private static String getNodeSequence(LinkedList<MyNode> linkedList) {
+
+    private static String getNodeSequence(LinkedList<MyNode<Integer>> linkedList) {
         StringBuilder sequence = new StringBuilder();
 
         for (MyNode node : linkedList) {
@@ -145,3 +174,6 @@ public class CreateLinkedlist {
 
     }
 }
+
+
+
--- src/com/bridgelabz/linkedlist/LinkedListSearchTest.java
+++ src/com/bridgelabz/linkedlist/LinkedListSearchTest.java
@@ -1,7 +1,4 @@
 package com.bridgelabz.linkedlist;
-
-
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.testng.AssertJUnit.assertEquals;
@@ -9,7 +6,6 @@ import static org.testng.AssertJUnit.assertNotNull;
 
 public class LinkedListSearchTest {
     @Test
-
     public void testSearch() {
         CreateLinkedlist list = new CreateLinkedlist();
 
@@ -18,17 +14,27 @@ public class LinkedListSearchTest {
         list.insert(30);
         list.insert(70);
 
-        // Test searching for node with value 30
+
+
+
+
         MyNode node30 = list.search(30);
+
+
         assertNotNull("Node with value 30 should be found", node30);
         assertEquals("Found node should have data value 30", 30, node30.data);
 
         list.insertAfter(node30, 40);
 
-
         list.delete(40);
+        CreateLinkedlist.SortedLinkedList<Integer> sortedList = new CreateLinkedlist.SortedLinkedList<>();
+        sortedList.add(56);
+        sortedList.add(30);
+        sortedList.add(40);
+        sortedList.add(70);
 
-        MyNode current = list.head;
+        sortedList.display();
+        MyNode<Integer> current = list.head;
         while (current != null) {
             System.out.print(current.data + "->");
             current = current.next;
